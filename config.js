e56ec6f2e127ec775ae6454e5afc33bd8751afa0
Emmanuel Ugwueze <emmanuelugwueze6@gmail.com>
2026-09-14 20:05:33 +0100

    update url

--- config.js
+++ config.js
@@ -1,2 +1,3 @@
 // Point this at wherever the backend API is running/deployed.
-const API_BASE_URL = 'http://localhost:3000';
+const API_BASE_URL = "https://hideout-menu-backend-fbea06161e33.herokuapp.com";
+// 'http://localhost:3000';
